4eecf2db0b308be375fa259eeed8dedf9d1baf61
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-23 00:10:51 +0300

    configured indentation and set type to module

--- SkillEx/vite.config.js
+++ SkillEx/vite.config.js
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
     plugins: [react()],
     server: {
-      port: 3000,
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
+        port: 3000,
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000",
+            },
         },
-      },
     },
-  
+
 })
