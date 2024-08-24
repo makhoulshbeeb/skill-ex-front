e0ce64186b3fdb2d80695153d4a68c1626ef63c3
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-24 23:40:12 +0300

    added reviewsApi

---
+++ SkillEx/src/api/ReviewsApi.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const reviewsApi = createApi({
+    reducerPath: 'reviewsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:5000/api/",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }),
+    tagTypes: ['Reviews'],
+    endpoints: (builder) => ({
+        getReviews: builder.query({
+            query: ({ receiver_id }) => `reviews/${receiver_id}`,
+            providesTags: (result, error, arg) =>
+                result
+                    ? [...result.map(({ id }) => ({ type: 'Reviews', id })), 'Reviews']
+                    : ['Reviews'],
+        }),
+        sendReview: builder.mutation({
+            query: (receiver_id, data) => ({
+                url: `reviews/${receiver_id}`,
+                method: 'POST',
+                body: data
+            }),
+            invalidatesTags: (result, error, arg) => [{ type: 'Reviews', id: arg.id }],
+        }),
+        deleteReview: builder.mutation({
+            query: ({ id }) => ({
+                url: `reviews/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: (result, error, arg) => [{ type: 'Reviews', id: arg.id }],
+        })
+    }),
+});
+
+export const {
+    useGetReviewsQuery,
+    useSendReviewMutation,
+    useDeleteReviewMutation
+} = reviewsApi;
