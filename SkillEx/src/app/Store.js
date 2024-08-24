be548c8b93999e86ccf622b4a809393ad578ebe3
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-24 23:53:25 +0300

    configured redux store and added api paths and middleware

---
+++ SkillEx/src/app/Store.js
@@ -0,0 +1,33 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { authApi } from "../api/AuthApi";
+import { usersApi } from "../api/UsersApi";
+import { messagesApi } from "../api/MessagesApi";
+import { chatsApi } from "../api/ChatsApi";
+import { reviewsApi } from "../api/ReviewsApi";
+
+const store = configureStore({
+    enhancers: (getDefaultEnhancers) =>
+        getDefaultEnhancers({
+            autoBatch: { type: 'tick' },
+        }),
+    reducer: {
+        [authApi.reducerPath]: authApi.reducer,
+        [usersApi.reducerPath]: usersApi.reducer,
+        [chatsApi.reducerPath]: chatsApi.reducer,
+        [messagesApi.reducerPath]: messagesApi.reducer,
+        [reviewsApi.reducerPath]: reviewsApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat([
+            authApi.middleware,
+            usersApi.middleware,
+            chatsApi.middleware,
+            messagesApi.middleware,
+            reviewsApi.middleware
+        ])
+})
+
+setupListeners(store.dispatch);
+
+export default store
