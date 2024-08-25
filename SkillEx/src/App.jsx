ac11cc1ff15c02fbc51a11ca014104d6e5cce33c
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-25 23:49:25 +0300

    created the css for left side of navbar

--- SkillEx/src/App.jsx
+++ SkillEx/src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+import Navbar from './components/Navbar.jsx'
+
 function App() {
 
     return (
-        <>
-
-        </>
+        <BrowserRouter>
+            <Navbar></Navbar>
+        </BrowserRouter>
     )
 }
 
--- SkillEx/src/components/Navbar.jsx
+++ SkillEx/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import "./styles/Navbar.css"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+    const logo = '/SkillEx Logo Light with Text.png'
     return (
         <header>
             <div className="left">
                 <nav>
-                    <Link><div className="logo"></div></Link>
+                    <img src={logo} className="logo"></img>
                     <Link>Home</Link>
                     <Link>Discover</Link>
                     <Link>Community</Link>
                     <Link>About Us</Link>
                     <Link>Contact Us</Link>
                 </nav>
+            </div>
+            <div className="right">
+
             </div>
         </header>
     )
