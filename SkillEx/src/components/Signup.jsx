1d768760b9a27a8c0fb86db1e806dbfe92911c85
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-27 00:44:19 +0300

    configured toasts

--- SkillEx/src/components/Signup.jsx
+++ SkillEx/src/components/Signup.jsx
@@ -1,37 +1,51 @@
 import { useNavigate } from "react-router-dom";
-import Button from "./Button";
+import { useForm } from "react-hook-form"
+import { useSignupMutation } from "../api/AuthApi";
+
+import toast from 'react-hot-toast';
 
 export default function Signup() {
     const navigate = useNavigate();
+    const form = useForm();
 
+    const { register, handleSubmit } = form;
+
+    const [signup, { data: res, isSuccess, isLoading, isError, error }] = useSignupMutation();
+
+    if (isLoading) {
+        toast.loading("Signing up...");
+    }
+    if (isError) {
+        toast.error(error.data.error);
+    }
+    if (isSuccess) {
+        toast.success("Welcome to SkillEx!");
+        setTimeout(() => { navigate('/') }, 1000)
+    }
+
+    const signupHandler = async (data, e) => {
+        console.log(e);
+        e.preventDefault();
+        signup(data);
+    }
 
     return (
         <div className="form-container">
             <h2 className="title">Sign up</h2>
             <hr />
-            <form className="input-form">
-                <input type="text" id={'displayName'} placeholder={'Display Name'} required />
-                <input type="text" id={'username'} placeholder={'Username'} required />
-                <input type="text" id={'email'} placeholder={'email@example.com'} required />
-                <input type="date" id={'date'} required />
+            <form className="input-form" onSubmit={handleSubmit(signupHandler)}>
+                <input type="text" id={'displayName'} placeholder={'Display Name'} {...register('displayName')} required />
+                <input type="text" id={'username'} placeholder={'Username'} {...register('username')} required />
+                <input type="text" id={'email'} placeholder={'email@example.com'} {...register('email')} required />
+                <input type="date" id={'date'} {...register('birthday')} required />
                 <div>
-                    <input type="radio" id={'male'} value={'male'} required />
-                    <input type="radio" id={'female'} value={'female'} required />
+                    <input type="radio" id={'male'} value={'male'} {...register('gender')} required />
+                    <input type="radio" id={'female'} value={'female'} {...register('gender')} required />
                 </div>
-                <input type="password" id={'password'} placeholder={'Password'} required />
-                <input type="password" id={'confirmPassword'} placeholder={'Confirm Password'} required />
-
+                <input type="password" id={'password'} placeholder={'Password'} {...register('password')} required />
+                <input type="password" id={'confirmPassword'} placeholder={'Confirm Password'} {...register('confirmPassword')} required />
+                <input type="submit" />
             </form>
-            <Button
-                bgColor="--primary-color"
-                text="Sign Up"
-                textColor="--background-color"
-                borderRadius="1rem"
-                borderColor="--primary-color"
-                onClick={() => {
-                    navigate("/auth/signup");
-                }}
-            ></Button>
             <div className="have-account">Don't have an account?<span onClick={() => navigate('/auth/signup')}>Sign up</span></div>
         </div>
     )
--- SkillEx/src/main.jsx
+++ SkillEx/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './main.css'
 import { Provider } from 'react-redux'
 import store from './app/Store.js'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
             <App />
+            <Toaster></Toaster>
         </Provider>
     </StrictMode>,
 )
