d6556b70437a7111cc21a7fb11618f1f3fdb22b5
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-29 02:38:11 +0300

    updated routes

--- SkillEx/src/App.jsx
+++ SkillEx/src/App.jsx
@@ -1,26 +1,26 @@
 
 import './App.css'
 
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes, useParams } from "react-router-dom";
 
 import Navbar from './components/Navbar.jsx'
 import Auth from './pages/Auth.jsx';
 
 function App() {
-
     return (
         <BrowserRouter>
-            <Navbar></Navbar>
             <Routes>
-                <Route path='/' />
+                <Route path='/home' element={<Navbar />}>
+                    <Route path='/home' />
+                    <Route path='/home/about' />
+                    <Route path='/home/discover' />
+                    <Route path='/home/community' />
+                    <Route path='/home/search/:search' />
+                    <Route path='/home/users/:username' />
+                    <Route path='/home/chats' />
+                    <Route path='/home/sessions' />
+                </Route>
                 <Route path='/auth/:sign' element={<Auth />} />
-                <Route path='/about' />
-                <Route path='/discover' />
-                <Route path='/community' />
-                <Route path='/search/:search' />
-                <Route path='/users/:username' />
-                <Route path='/chats' />
-                <Route path='/sessions' />
             </Routes>
         </BrowserRouter>
     )
