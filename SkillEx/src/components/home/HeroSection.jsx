0c275c1ae8777ffb15d8cdf8c5366a04467088ad
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-05 01:35:27 +0300

    established infrastructure for hero section

--- SkillEx/src/components/home/HeroSection.jsx
+++ SkillEx/src/components/home/HeroSection.jsx
@@ -3,6 +3,13 @@ export default function HeroSection() {
     return (
         <div className="hero-section">
             <img src={'/SkillEx Background 1.png'} alt="SkillEx Hero Section Background" />
+            <div>
+                <img src={'/Hero image.png'} alt="Hero Image" />
+                <div className='hero-slogan'>
+                    <h1>Unleash Potential, Share Skills, Grow Together</h1>
+                    <p>Connect with passionate individuals from diverse backgrounds, share your expertise, and learn new skills that elevate your potential.</p>
+                </div>
+            </div>
         </div>
     )
 }
