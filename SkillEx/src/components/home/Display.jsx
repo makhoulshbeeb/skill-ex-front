61e08595066726fba9fd4bfd33a00ec5ff75eb71
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-06 00:50:13 +0300

    made display into a generalized component for different types of display

--- SkillEx/src/components/home/Display.jsx
+++ SkillEx/src/components/home/Display.jsx
@@ -3,17 +3,18 @@ import CategoryDisplayPanel from './CategoryDisplayPanel';
 
 
 export default function Display({ displayTitle, items, type }) {
-    const categories = [{ name: "Category 1", picture: "https://e7.pngegg.com/pngimages/75/866/png-clipart-category-management-organization-retail-management-miscellaneous-text-thumbnail.png" }];
 
-    if (categories) {
+    if (items) {
         return (
             <div className="display">
                 <h2>{displayTitle}</h2>
                 <div className="scroll-display">
-                    {categories.map(category => {
-                        return (
-                            <CategoryDisplayPanel category={category} key={category.id} />
-                        )
+                    {items.map(item => {
+                        if (type == 'category') {
+                            return (
+                                <CategoryDisplayPanel category={item} key={item.id} />
+                            )
+                        }
                     }
 
                     )}
