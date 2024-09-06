d33ce64af1ca58341d6834cab267a9f9eaed56ce
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-06 03:11:18 +0300

    added explore basic header with style

--- SkillEx/src/pages/Explore.jsx
+++ SkillEx/src/pages/Explore.jsx
@@ -1,10 +1,25 @@
+import "./styles/Explore.css"
+
+import { useParams } from "react-router-dom";
 import Navbar from "../components/common/Navbar";
+import Searchbar from "../components/common/Searchbar";
 
 export default function Explore() {
+    const { type } = useParams();
     return (
-        <div className="explore-page">
+        <>
             <img src="/SkillEx Background 4.png" alt="Explore Page Background" className="bg" />
             <Navbar />
-        </div>
+            <div className="explore-page">
+                <div>
+                    <div className="explore-header">
+                        <h1>{type}</h1>
+                        <Searchbar placeholder={`Search ${type}`} />
+                    </div>
+                    <hr />
+                </div>
+
+            </div>
+        </>
     )
 }
