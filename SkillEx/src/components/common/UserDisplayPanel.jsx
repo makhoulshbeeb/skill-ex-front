129d73e1cc41c830744d536e4913a97a9476a9cc
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-08 02:36:27 +0300

    stylized user display panel

--- SkillEx/src/components/common/UserDisplayPanel.jsx
+++ SkillEx/src/components/common/UserDisplayPanel.jsx
@@ -9,13 +9,15 @@ export default function UserDisplayPanel({ user }) {
     const navigate = useNavigate();
     return (
 
-        <div onClick={() => { navigate(`/user/${user.username}`) }} className="user-panel">
-            <div className="user-picture" style={{ backgroundImage: 'url(/User Panel Background.png)' }}>
-                <img src={`${user.picture}`} />
+        <div className="user-panel" onClick={() => { navigate(`/user/${user.username}`) }} >
+            <div className="user-panel-picture" style={{ backgroundImage: 'url(/User Panel Background.png)' }}>
+                <img src={`${user.picture}`} alt={`${user.username}'s Picture`} />
             </div>
-            <div>
-                <h2>{user.displayName}</h2>
-                <h3>{user.learn[0].category.name}</h3>
+            <div className="user-panel-details">
+                <div>
+                    <h2>{user.displayName}</h2>
+                    <h3>{user.learn[0].category.name}</h3>
+                </div>
                 <div className="user-panel-rating">
                     {[1, 2, 3, 4, 5].map((el) => {
                         if (user.avgRating - el >= 0) {
@@ -27,7 +29,7 @@ export default function UserDisplayPanel({ user }) {
                         }
 
                     })}
-                    <span>{Math.round(user.avgRating * 10) / 10}</span>
+                    <span>&nbsp;&nbsp;{Math.round(user.avgRating * 10) / 10}</span>
                 </div>
                 <p>{user.bio}</p>
             </div>
