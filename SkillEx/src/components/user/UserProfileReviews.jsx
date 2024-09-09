08bd8e0d67fa74e8f08ce16da476b2c508315ddb
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-09 15:31:51 +0300

    stylized user profile reviews

--- SkillEx/src/components/user/UserProfileReviews.jsx
+++ SkillEx/src/components/user/UserProfileReviews.jsx
@@ -9,32 +9,36 @@ export default function UserProfileReviews({ reviews }) {
     return (
         <div className='user-profile-reviews'>
             <h2>Reviews</h2>
-            {reviews.map(review => {
-                console.log(review.rating)
-                return (
-                    <div className='review-container' key={review._id}>
-                        <div className='reviewer-tag'>
-                            <img src="https://avatar.iran.liara.run/public/boy?username=potentialwoo" />
-                            <div>
-                                <h4>{review.reviewerId}</h4>
-                                <div className="user-page-rating">
-                                    {[1, 2, 3, 4, 5].map((el) => {
-                                        if (review.rating - el >= 0) {
-                                            return (<FontAwesomeIcon icon={faStar} color="gold" />)
-                                        } else if (review.rating - el <= -1) {
-                                            return (<FontAwesomeIcon icon={faStarOutline} color="gold" />)
-                                        } else {
-                                            return (<FontAwesomeIcon icon={faStarHalfStroke} color="gold" />)
-                                        }
+            <div>
+                <div>
+                    {reviews.map(review => {
+                        console.log(review.rating)
+                        return (
+                            <div className='review-container' key={review._id}>
+                                <div className='reviewer-tag'>
+                                    <img src={review.reviewerId.picture} />
+                                    <div>
+                                        <h4>{review.reviewerId.displayName}</h4>
+                                        <div className="user-page-rating">
+                                            {[1, 2, 3, 4, 5].map((el) => {
+                                                if (review.rating - el >= 0) {
+                                                    return (<FontAwesomeIcon icon={faStar} color="gold" />)
+                                                } else if (review.rating - el <= -1) {
+                                                    return (<FontAwesomeIcon icon={faStarOutline} color="gold" />)
+                                                } else {
+                                                    return (<FontAwesomeIcon icon={faStarHalfStroke} color="gold" />)
+                                                }
 
-                                    })}
+                                            })}
+                                        </div>
+                                    </div>
                                 </div>
+                                <p>{review.feedback}</p>
                             </div>
-                        </div>
-                        <p>{review.feedback}</p>
-                    </div>
-                )
-            })}
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
