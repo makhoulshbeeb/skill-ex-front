b202a72b1f58023393b2326574b03d893780fc43
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-09 03:23:34 +0300

    added send message button

--- SkillEx/src/components/user/UserSidePanel.jsx
+++ SkillEx/src/components/user/UserSidePanel.jsx
@@ -1,14 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetUserByUsernameQuery } from "../../api/UsersApi";
 import { useSocketContext } from "../../context/SocketContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faMessage } from "@fortawesome/free-solid-svg-icons";
 
 export default function UserSidePanel() {
     const { onlineUsers } = useSocketContext();
+    const navigate = useNavigate();
 
     const { username } = useParams();
     const { data: user, isLoading, isSuccess, isError, error } = useGetUserByUsernameQuery({ username });
 
-    const online = onlineUsers.includes(user._id);
+    const online = onlineUsers.includes(user ? user._id : "");
     return (
         <div className="user-side-panel">
             {isSuccess && <>
@@ -18,9 +21,19 @@ export default function UserSidePanel() {
                         <div style={{ backgroundColor: online ? 'limegreen' : 'var(--text-light)' }}></div>
                     </div>
                 </div>
-                <div>
-                    <h2>{user.displayName}</h2>
+                <div className="user-page-contact">
+                    <div>
+                        <h2>{user.displayName}</h2>
+                        <FontAwesomeIcon
+                            icon={faEnvelope}
+                            fontSize={"1.2rem"}
+                            color="var(--background-color)"
+                            style={{ backgroundColor: "var(--primary-color)", padding: "0.5rem 0.5rem", borderRadius: "100%" }}
+                            onClick={(e) => navigate("/chats")}
+                        ></FontAwesomeIcon>
+                    </div>
                     <h3>@{user.username}</h3>
+
                 </div>
                 <br></br>
                 <p>{user.bio}</p>
