43730f2f64dcdcf0cd9668d1d5ce697e11e24c99
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-13 02:49:29 +0300

    created basic setup for video bar

--- SkillEx/src/components/video/VideoBar.jsx
+++ SkillEx/src/components/video/VideoBar.jsx
@@ -1,4 +1,4 @@
-import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
+import { faCamera, faMicrophone, faMicrophoneSlash, faPhone, faShareSquare, faVideo, faVideoSlash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 
@@ -6,8 +6,23 @@ export default function VideoBar() {
     const [mic, setMic] = useState(false);
     const [camera, setCamera] = useState(false);
     const [screenShare, setScreenShare] = useState(false);
+
+    const endCall = () => { }
     return (
         <div className='video-bar'>
+            <div style={{ backgroundColor: mic ? 'var(--text-light)' : 'tomato' }} onClick={() => setMic(!mic)}>
+                <FontAwesomeIcon icon={mic ? faMicrophone : faMicrophoneSlash} />
+            </div>
+            <div style={{ backgroundColor: camera ? 'var(--text-light)' : 'tomato' }} onClick={() => setCamera(!camera)}>
+                <FontAwesomeIcon icon={camera ? faVideo : faVideoSlash} />
+            </div>
+            <div style={{ backgroundColor: screenShare ? 'var(--primary-color)' : 'var(--text-light)' }} onClick={() => setScreenShare(!screenShare)} >
+                <FontAwesomeIcon icon={faShareSquare} />
+            </div>
+
+            <div style={{ backgroundColor: 'tomato' }} onClick={() => endCall()} >
+                <FontAwesomeIcon icon={faPhone} />
+            </div>
         </div>
     )
 }
