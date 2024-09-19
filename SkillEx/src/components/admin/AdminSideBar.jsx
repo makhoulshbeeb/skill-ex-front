033d123f71874edff2559cf481d314456279aeaf
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-19 22:50:23 +0300

    added some admin page components

--- SkillEx/src/components/admin/AdminSideBar.jsx
+++ SkillEx/src/components/admin/AdminSideBar.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function AdminSideBar({ setPanel }) {
     return (
-        <div className='admin-sidebar'>AdminSideBar</div>
+        <div className='admin-sidebar'>
+            <div className="admin-sidebar-header">
+                <FontAwesomeIcon
+                    icon={faArrowLeft}
+                    fontSize={"1.5rem"}
+                    color="var(--background-color)"
+                    style={{ backgroundColor: "var(--primary-color)", padding: "0.5rem 0.6rem", borderRadius: "1.5rem", cursor: "pointer" }}
+                    onClick={(e) => navigate(-1)}
+                />
+                <div>Admin</div>
+            </div>
+        </div>
     )
 }
